63dba0a314b6780c21a08ac3845f89be6cd7851c
Damian Kudosz <damiankudosz1990@gmail.com>
2025-12-18 08:32:06 +0100

    Build Fix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   typedRoutes: true,
   reactStrictMode: true,
   poweredByHeader: false,
-  eslint: {
-    // Avoid blocking Vercel builds on ESLint plugin resolution issues
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
